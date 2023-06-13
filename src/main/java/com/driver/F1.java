bf913277092b52fa890cbcae4acfe23da8e8b24f
CodeWithPrateekk <pru151998@gmail.com>
2023-06-13 19:20:57 +0530

    assignment

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -2,12 +2,20 @@ package com.driver;
 
 public class F1 extends Car {
 
-    public F1(String name, boolean isManual) {
-        //Use arbitrary values for parameters which are not mentioned
+    public F1() {
+        //super();
+    }
+
+    public F1(String type, boolean isManual) {
+           super.setName(type);
+           super.setManual(isManual);
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int k = super.getCurrentSpeed();
+        k+= rate;
+        super.setCurrentSpeed(k);
+        //set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -18,13 +26,31 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
-        if(newSpeed == 0) {
+        if(super.getCurrentSpeed() == 0) {
             //Stop the car, set gear as 1
+            super.changeGear(1);
+
         }
         //for all other cases, change the gear accordingly
 
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
+        if(super.getCurrentSpeed() > 0 && super.getCurrentSpeed() <51) {
+            super.changeGear(2);
+        }
+
+        if(super.getCurrentSpeed() >= 101 && super.getCurrentSpeed() <= 150) {
+            super.changeGear(3);
+        }
+
+        if(super.getCurrentSpeed() >= 151 && super.getCurrentSpeed() <=200) {
+            super.changeGear(4);
+        }
+
+        if(super.getCurrentSpeed() >= 201 && super.getCurrentSpeed() <=250) {
+            super.changeGear(5);
+        }
+
+        if(super.getCurrentSpeed() >= 250) {
+            super.changeGear(6);
         }
     }
 }
